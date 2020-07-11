a8c3724d773fb6c4e4815ef11888b10882aee20e
Junior Aparecido Rodrigues da Cruz <cin_acruz@uolinc.com>
2020-07-11 17:02:36 -0300

    Fazendo os testes falharem

--- src/test/java/br/com/jrprojetos/SpringDockerApplicationTests.java
+++ src/test/java/br/com/jrprojetos/SpringDockerApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.jrprojetos;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SpringDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 2);
 	}
 
 }
